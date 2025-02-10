0251a772c92bcdf17b390fc32a0ce4bf0eb791c0
karangudha <karankgudha@gmail.com>
2025-02-10 11:44:34 +0530

    add user routes for password change, account updates, avatar and cover image uploads, user profile retrieval, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUsercoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +38,12 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router.route(("/avatar-update").patch(verifyJWT, upload.single("avatar"), updateUserAvatar));
+router.route(("/cover-image-update").patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage));
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+//here we are getting details form link that why we have to do like this.
+router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
